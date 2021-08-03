81e48d1e00d922dd5e808ec88679335282d9bb83
Anuj Bhatnagar <bonjour.anuj@gmail.com>
2021-08-03 21:28:05 +0530

    Adds a sample unit test
    Adds a sample test to generation test and code coverage reports and check that sonarqube reflects the data.

---
+++ src/test/java/io/lazybird/temp/SampleClassTest.java
@@ -0,0 +1,13 @@
+package io.lazybird.temp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleClassTest {
+
+  @Test
+  void sampleMethod() {
+    assertTrue(SampleClass.sampleMethod());
+  }
+}
