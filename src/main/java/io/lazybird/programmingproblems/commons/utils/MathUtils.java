e3221aecdf07423ecd1770972bdc1e24d87a067e
Anuj Bhatnagar <bonjour.anuj@gmail.com>
2021-08-30 16:12:54 +0530

    Fixed bug: gcd of numbers with value 0

--- src/main/java/io/lazybird/programmingproblems/commons/utils/MathUtils.java
+++ src/main/java/io/lazybird/programmingproblems/commons/utils/MathUtils.java
@@ -2,15 +2,22 @@ package io.lazybird.programmingproblems.commons.utils;
 
 public class MathUtils {
 
+  private MathUtils() {
+  }
+
   /**
    * Finds the greatest common divisor of two non-zero numbers.
    *
    * @return gcd of x & y
    */
-  public static int gcd(int x, int y) {
-    //TODO - assert that x & y are not equals to 0;
-    int max = Math.max(x, y);
-    int min = Math.min(x, y);
+  public static int gcd(int x, int y) throws IllegalArgumentException {
+
+    if (x == 0 || y == 0) {
+      throw new IllegalArgumentException(
+          String.format("x=%d, y=%d should be non-zero integers", x, y));
+    }
+    int max = Math.max(Math.abs(x), Math.abs(y));
+    int min = Math.min(Math.abs(x), Math.abs(y));
 
     if (max == min) {
       return max;
--- src/test/java/io/lazybird/programmingproblems/commons/utils/MathUtilsTest.java
+++ src/test/java/io/lazybird/programmingproblems/commons/utils/MathUtilsTest.java
@@ -1,9 +1,12 @@
 package io.lazybird.programmingproblems.commons.utils;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,11 +19,22 @@ class MathUtilsTest {
     assertEquals(MathUtils.gcd(x, y), gcd);
   }
 
+  @Test
+  void gcdFailedCases() {
+    Exception e = catchThrowableOfType(() -> {
+      MathUtils.gcd(0, 10);
+    }, IllegalArgumentException.class);
+
+    assertThat(e).hasMessageContaining("should be non-zero integers");
+  }
+
   static Stream<Arguments> gcdTestCases() {
     return Stream.of(arguments(2, 4, 2),
         arguments(1, 3, 1),
         arguments(10, 5, 5),
-        arguments(10, 4, 2)
+        arguments(10, 4, 2),
+        arguments(4, 4, 4),
+        arguments(-10, 4, 2)
     );
   }
 }
